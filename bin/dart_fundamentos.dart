230c7d42b138b82ef1770b0ae193a648f5cf46a9
Felipe Sales <soufeliposales@gmail.com>
2021-09-23 05:59:57 -0300

    initial commit

---
+++ bin/dart_fundamentos.dart
@@ -0,0 +1,5 @@
+import 'package:dart_fundamentos/dart_fundamentos.dart' as dart_fundamentos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_fundamentos.calculate()}!');
+}
---
+++ test/dart_fundamentos_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_fundamentos/dart_fundamentos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
