7b8461f4c6df68122ab81f096c49243a0569cf87
Felipe Sales <soufeliposales@gmail.com>
2021-10-01 05:27:46 -0300

    conditional operators | Part 01

---
+++ lib/03_operators/01_conditionals.dart
@@ -0,0 +1,12 @@
+void main() {
+  final age = 18;
+
+  // age can > 18 to do true
+  if (true) {
+    print('true');
+  } else if (age == 18) {
+    print('true');
+  } else {
+    print('no');
+  }
+}
---
+++ lib/03_operators/02_relationals.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Relationals Operators
+  // ==,  !=, >, <, <=, >=
+  // check business rule
+
+  final typePet = 'Cat';
+  final age = 18;
+  if (age == 18) {
+    print('okay!');
+  }
+
+  if (age > 18) {
+    print('okay!');
+  }
+
+  if (age < 18) {
+    print('No!');
+  }
+
+  if (typePet != 'Dog') {
+    print('Sorry! I have only dog');
+  }
+}
