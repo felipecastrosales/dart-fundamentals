74ca274732ab3d26b7c59e7d4f29ed95c031f461
Felipe Sales <soufeliposales@gmail.com>
2021-09-30 23:22:18 -0300

    passing the code of the class to english

--- lib/01_variaveis/01_aula.dart
+++ lib/01_variaveis/01_aula.dart
@@ -1,30 +1,35 @@
 void main() {
-  // tem tipo, o nome e a atribuição de valor pra variável
+  // have type, the name and atribuction value for variable
 
   // inteiros: 1, 2, 3...
-  int idade = 17;
-  // decimais: 1.0, 2.0, 3.0...
-  double valor = 7.00;
-  // num -> aceita ambos os tipos
-  num qualquerNumero = 1.2;
+  int age = 17;
+  
+  // decimals: 1.0, 2.0, 3.0...
+  double value = 7.00;
+  
+  // num -> accept all types (double - int)
+  num anyNum = 1.2;
 
-  // textos
-  String nome = 'Felipe';
+  // texts
+  String name = 'Felipe';
 
-  // seguir camelCase
-  // tudo no dart é classe
+  // booleans: true or false
+  bool isLoading = true;
 
-  // palavra reservada var
-  // var detecta a tipagem da sua variável de acordo com o valor atribuído 
-  // não consegue inferir no num, pois ele vai interpretar como int ou double
+  // follow camelCase
+  // in dart all is classes
 
-  // Object -> todas as classes do nosso sistema herdam de object
-  // pode ser qualquer coisa (tipo), pois todos são filhos de Object
-  Object objetoQualquer = '';
+  // keyword 'var'
+  // var detects the type of the variable with your value atributed 
+  // don't put in 'num' type, because he understanding a int or double
+
+  // Object -> all classes in system are inherit from Object
+  // it could be anything (type), since they are all children of Object
+  Object anyObject = '';
 
   // dynamic -> parece com Object
-  dynamic dynamicQualquer = 'Dart';
+  dynamic anyDynamic = 'Dart';
   // podemos mudar o estado da variável com uma atribuição sem o var
-  dynamicQualquer = 2;
+  anyDynamic = 2;
 
 }
