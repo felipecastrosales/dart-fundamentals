9391c4ac23736d5c1020da69d8d93404988e0c98
Felipe Sales <soufeliposales@gmail.com>
2021-10-10 13:30:20 -0300

    adding final module challenge

---
+++ lib/08_desafio/01_desafio.dart
@@ -0,0 +1,38 @@
+void main() {
+  // ! Detail about String
+  // ! String has 4 fields (Name|Age|Work|State|)
+
+  final people = [
+    'Felipe Sales|17|Developer|CE',
+    'Karol Barroso|17|Marketing|CE',
+    'Maverick Alekyne Ribeiro|17|Vagabundo|CE',
+    'Daniel Vaz|17|All|CE',
+    'Rebeca Sales|22|Doctor Student|SP',
+    'Shadai Sales|20|Merchant Marine|PR',
+    'Pedro Yago|18|Student|CE',
+    'Pedro Yago|18|Student|CE',
+    'Gustavo Tito|12|Student',
+    'Yerich Sales|43|developer|CE',
+  ];
+
+  //! Based on the matrix above, create a report where:
+  // ! | 1 | Present people with more 18 years and print this
+  // ! | 2 | Present people quantity have in profission ()
+  // ! | 3 | Present quantity person who live in Ceará
+
+  // 1
+  // person with more 20 years
+    // Rebeca
+    // Shadai
+
+  // 2
+  // Developer
+    // Felipe
+    // Yerich
+
+  // 3
+  // lives in Ceará
+    // Felipe
+    // Yerich
+
+}
