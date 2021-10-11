894e147ef88a2a3c946a2af506a2d76c9c0aaa2a
Felipe Sales <soufeliposales@gmail.com>
2021-10-10 22:43:37 -0300

    working with functions | part 01

---
+++ lib/02_functions_collections/01_functions/01_functions.dart
@@ -0,0 +1,11 @@
+void main() {
+  final sumValue = sum(10, 10);
+  print('The value is: $sumValue');
+  //print('The value is: ${sum(10,  10)}');
+}
+
+int sum(int num1, int num2) {
+  // final sumNum = num1 + num2;
+  // return sumNum;
+  return num1 + num2;
+}
