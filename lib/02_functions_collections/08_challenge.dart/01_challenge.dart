f9b38d4f86906bb9f74cecb4dd7ceaa76e34d6ea
Felipe Sales <soufeliposales@gmail.com>
2021-10-13 22:56:16 -0300

    adding template challenge

---
+++ lib/02_functions_collections/08_challenge.dart/01_challenge.dart
@@ -0,0 +1,25 @@
+void main() {
+  //! Detail about String
+  //! String has 3 fields (name|age|sex)
+  final people = [
+    'Felipe Sales|17|Masculine',
+    'Karol Barroso|17|Feminine',
+    'Felipe Sales|17|Masculine',
+    'Maverick Alekyne Ribeiro|17|Masculine',
+    'Daniel Vaz|17|Masculine',
+    'Daniel Vaz|17|Masculine',
+    'Rebeca Sales|22|Feminine',
+    'Karol Barroso|17|Feminine',
+    'Shadai Sales|20|Masculine',
+    'Pedro Yago|18|Masculine',
+    'Gustavo Tito|12|Masculine',
+    'Pedro Yago|18|Masculine',
+    'Yerich Sales|43|Masculine',
+  ];
+
+  //! Based on the list above
+  // ! | 1 | Remove duplicate patients and present the new list
+  // ! | 2 | Show me the amount of people per sex (Male and Female) and then show me their names
+  // ! | 3 | Filter and leave the list only with people over 18 years old and present these people by name
+  // ! | 4 | Find the oldest person and present their name.
+}
